c7a6954725008ae06613344f914af2b9ed5e74a0
Kevin Huang <yellow.khaos@gmail.com>
2019-02-07 13:35:01 +0000

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,16 @@
+function removeFriend (array, id) {
+  return array.filter(function(value, index, arr){
+    return value.id != id
+  })
+}
+
 export function manageFriends(state, action){
+  switch(action.type) {
+    case "ADD_FRIEND" :
+      return {friends: [...state.friends, action.friend]}
+    case "REMOVE_FRIEND" :
+      return {friends: removeFriend(state.friends, action.id)}
+    default:
+      return state
+  }
 }
