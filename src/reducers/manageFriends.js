12576772aa6b3835e4bcd6c1990dde6f3bffbc83
Kevin Huang <yellow.khaos@gmail.com>
2019-02-07 13:12:44 +0000

    Automatically backed up by Learn

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,8 @@
 export function manageFriends(state, action){
+  switch(action.type) {
+    case "ADD_FRIEND" :
+      return {friends: [...state.friends, action.friend]}
+    default:
+      return state
+  }
 }
