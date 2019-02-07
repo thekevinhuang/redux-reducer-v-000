e9e9ba1878592a580c1cb0e67abe1ea3c40c76b4
Kevin Huang <yellow.khaos@gmail.com>
2019-02-07 13:16:18 +0000

    Automatically backed up by Learn

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,9 +1,13 @@
+function friendFind (element, id) {
+  return element.id == id
+}
+
 export function manageFriends(state, action){
   switch(action.type) {
     case "ADD_FRIEND" :
       return {friends: [...state.friends, action.friend]}
     case "REMOVE_FRIEND" :
-      return {friends: }
+      return {friends: state.friends.findIndex()}
     default:
       return state
   }
